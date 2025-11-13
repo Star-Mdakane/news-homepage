fc5078d5ac50746b0a51468caf33c72cf3eccb66
Star-Mdakane <101474151+Star-Mdakane@users.noreply.github.com>
2025-11-13 06:30:53 +0200

    Update script.js menu overlay functionality

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ const closeMenuBtn = document.getElementById('close');
 const menu = document.getElementById('menu');
 const burger = document.getElementById('burger');
 
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.appendChild('overlay');
+
 const openMenu = () => {
     menu.classList.add('open');
+    overlay.classList.add('active');
+    document.body.style.overflow = 'hidden';
 }
 
 const closeMenu = () => {
     menu.classList.remove('open');
+    overlay.classList.remove('active');
+    document.body.style.overflow = ' ';
 }
 
 burger.addEventListener('click', openMenu);
 closeMenuBtn.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && menu.classList.contains('open')) {
+    closeMenu();
+  }
+
